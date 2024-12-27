8e71db29192ca7d93d7b4e8d66825d06096cfb10
Evgeniy Sakov <91192469+PiPuKaPRo@users.noreply.github.com>
2024-12-27 23:04:37 +0300

    Delete CRS-sec directory

--- CRS-sec/src/main/java/ru/vsu/cs/sakovea/repository/UserCompPermsRepository.java
+++
@@ -1,9 +0,0 @@
-package ru.vsu.cs.sakovea.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import ru.vsu.cs.sakovea.models.UserCompPerm;
-
-@Repository
-public interface UserCompPermsRepository extends JpaRepository<UserCompPerm, Integer> {
-}
