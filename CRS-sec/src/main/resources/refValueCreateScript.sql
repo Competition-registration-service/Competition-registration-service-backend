380c17fbddc2d616608cf44a87101c0540613234
PiPuKaPRo <evg_s_zheniy@mail.ru>
2025-03-22 16:06:52 +0300

    some refactor

---
+++ CRS-sec/src/main/resources/refValueCreateScript.sql
@@ -0,0 +1,6 @@
+
+--Универсальный скрипт для добавления нового значения в словарь--
+
+INSERT INTO ref_value (domain_cid, value_cid, short_value, long_value, visible, comment)
+VALUES
+    ('', '', '', '', true, '');
